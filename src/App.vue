0cc5c4eda266f29193a1e43ef0af068b6e4dc58f
xmyhero1995x <62993845+xmyhero1995x@users.noreply.github.com>
2021-11-25 22:10:46 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <button @click="showModal = true" >Show modal</button>
+    <Modal :showModal="showModal" @close-modal="showModal = false"></Modal>
+  </div>
+</template>
+
+<script>
+import Modal from "./components/Modal.vue"
+
+
+export default {
+  name: "App",
+  data() {
+    return {
+      showModal: false
+    };
+  },
+  components: {
+    Modal
+  },
+};
+</script>
+
+<style>
+#app {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 30px;
+}
+</style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="modal" v-if="showModal">
+    <div class="modal-block">
+      <button type="button" @click="closeModal">Close</button>
+    </div>
+  </div>
+</template>
+
+
+<script type="text/javascript">
+export default {
+    props: ['showModal'],
+    methods: {
+        closeModal() {
+            this.$emit('close-modal')
+        }
+    }
+}
+</script>
+
+
+<style>
+.modal {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: grey;
+  width: 100%;
+  height: 100vh;
+  position: fixed;
+  top: 0;
+  left: 0;
+}
+.modal-block {
+  width: 600px;
+  background: white;
+  height: 300px;
+  border-radius: 4px;
+  margin: 0 auto;
+  padding: 10px;
+  box-sizing: border-box;
+}
+.modal-block button {
+  margin-left: auto;
+  margin-right: 0;
+  display: block;
+
+}
+</style>
